c010065629f548a44cb6c479a8867085b7f30b77
Hungf72 <quochung121005@icloud.com>
2025-05-27 22:00:18 +0700

    Update notes.php

--- Note-Web/controllers/notes.php
+++ Note-Web/controllers/notes.php
@@ -3,7 +3,7 @@
     require_once __DIR__ . '/../models/connect.php';
 
     header('Content-Type: application/json');
-
+    
     function getNOTES(){
         global $pdo;
         $userId = $_SESSION['user']['id'] ?? null;
@@ -100,6 +100,32 @@
                 echo json_encode(['status' => 'error', 'message' => 'Lỗi hệ thống: ' . $e->getMessage()]);
             }
             exit;
+        }
+    }
+
+    function autoSaveNOTES() {
+        global $pdo;
+        $noteIT = $_POST['id'] ?? null;
+        $title = trim($_POST['title'] ?? '');
+        $content = trim($_POST['content'] ?? '');
+        $userId = $_SESSION['user']['id'] ?? null;
+
+        if (!$userId) {
+            echo json_encode(['status' => 'error', 'message' => 'Bạn cần đăng nhập để tự động lưu note.']);
+            exit;
+        }
+
+        if (!$noteIT) {
+            echo json_encode(['status' => 'error', 'message' => 'ID note không hợp lệ.']);
+            exit;
+        }
+
+        try{
+            $stmt = $pdo->prepare(("UPDATE notes SET title = :title, content = :content WHERE id = :id AND user_id = :user_id"));
+            $stmt->execute((['title' => $title, 'content' => $content, 'id' => $noteIT, 'user_id' => $userId]));
+            echo json_encode(['status' => 'success', 'message' => 'Note đã được tự động lưu thành công.']);
+        } catch (PDOException $e) {
+            echo json_encode(['status' => 'error', 'message' => 'Lỗi hệ thống: ' . $e->getMessage()]);
         }
     }
 
