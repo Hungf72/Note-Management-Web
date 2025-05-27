1e6f4133a5c5d799f02ca2224d7cb7520867f7d3
Hungf72 <quochung121005@icloud.com>
2025-05-27 14:54:50 +0700

    Update noteSQL.sql

--- Note-Web/noteSQL.sql
+++ Note-Web/noteSQL.sql
@@ -12,11 +12,11 @@ create table Users(
 )
 
 CREATE TABLE IF NOT EXISTS notes (
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    user_id INT NOT NULL,
-    title VARCHAR(255) NOT NULL,
-    content TEXT,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    last_modified TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    id int primary key AUTO_INCREMENT,
+    user_id int not null,
+    title varchar(255) not null,
+    content text not null,
+    created_at timestamp default CURRENT_TIMESTAMP,
+    last_modified timestamp default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP,
     FOREIGN KEY (user_id) REFERENCES users(id)
 );
