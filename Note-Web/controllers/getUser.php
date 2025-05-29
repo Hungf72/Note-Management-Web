8a2b2494e8a356cecc96eec0539f56c4a300d3c6
KyleHuynh0910 <huygia9105@gmail.com>
2025-05-29 23:05:30 +0700

    Create getUser.php

---
+++ Note-Web/controllers/getUser.php
@@ -0,0 +1,23 @@
+<?php
+session_start();
+header('Content-Type: application/json');
+
+if (!isset($_SESSION['user'])) {
+    echo json_encode([
+        'status' => 'error',
+        'message' => 'Chưa đăng nhập.'
+    ]);
+    exit;
+}
+
+$user = $_SESSION['user'];
+
+echo json_encode([
+    'status' => 'success',
+    'email' => $user['email'],
+    'firstName' => $user['firstName'],
+    'lastName' => $user['lastName'],
+    'age' => $user['age'],
+    'phone' => $user['phone']
+]);
+?>
